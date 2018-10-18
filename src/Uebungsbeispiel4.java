425d8190f80a6437105427d7056ff3477475c271
thourl <thourl@L-4Y8TXY1.cu.lannach>
2018-10-18 12:28:55 +0200

    Amount negative

--- src/Uebungsbeispiel4.java
+++ src/Uebungsbeispiel4.java
@@ -23,56 +23,58 @@ public class Uebungsbeispiel4 {
 		double meanAbsolute = 0;
 		int delta = 0;
 		long sumMinMax = 0;
-		
+
 		Scanner sc = new Scanner(System.in);
 
-		System.out.print("Amount: ");
-		
-		if (sc.hasNextInt()) {
-			amount = sc.nextInt();
+		while (true) {
+			System.out.print("Amount: ");
+			if (sc.hasNextInt()) {
+				amount = sc.nextInt();
+			} else
+				sc.next();
+			if (amount > 0)
+				break;
 			// Go into while
-		
-			int count = 1;
-			while (count <= amount) {
-				System.out.print("#" + count + ": ");
-			
-				if (sc.hasNextInt()) {
-					enteredValue = sc.nextInt();
-					summe += enteredValue;
-					if (enteredValue < min) {
-						min = enteredValue;
-					}
-
-					if (enteredValue > max) {
-						max = enteredValue;
-					}
-					count++;
-				} else {
-					sc.next();
-				}
-			}
-
-			mean = summe * 1.0 / amount * 1.0; // Prof Fragen Warum wenn ich summe * 1.0 rechne richtiges ergebnis?
-			meanReciprocal = 1 / mean;
-			meanNegative = mean * -1;
-			delta = max - min;
-			meanAbsolute = (mean > 0) ? (meanAbsolute = mean) : (meanAbsolute = mean * -1);
-			sumMinMax = (min + max) * (delta + 1) / 2;
-
-			System.out.println("Sum = " + summe);
-			System.out.println("Minimum = " + min);
-			System.out.println("Maximum = " + max);
-			System.out.printf("Mean = %4.2f\n", mean);
-			System.out.printf("Reciprocal of mean = %4.2f\n", meanReciprocal);
-			System.out.printf("Additive inverse of mean = %4.2f\n", meanNegative);
-			System.out.printf("Absolute value of mean = %4.2f\n", meanAbsolute);
-			System.out.println("Delta of minimum and maximum = " + delta);
-			System.out.println("Sum of numbers from minimum to maximum = " + sumMinMax);
-
-		} else {
-			System.out.println("Bitte eine Ganze Zahl eingeben");
-			sc.next();
 		}
+		int count = 1;
+		while (count <= amount) {
+			System.out.print("#" + count + ": ");
+
+			if (sc.hasNextInt()) {
+				enteredValue = sc.nextInt();
+				summe += enteredValue;
+				if (enteredValue < min) {
+					min = enteredValue;
+				}
+
+				if (enteredValue > max) {
+					max = enteredValue;
+				}
+				count++;
+			} else {
+				sc.next();
+			}
+		}
+		
+	
+
+		mean = 1.0 * summe / amount; // Prof Fragen Warum wenn ich summe * 1.0 rechne richtiges ergebnis?
+		meanReciprocal = 1 / mean;
+		meanNegative = mean * -1;
+		delta = max - min;
+		meanAbsolute = (mean > 0) ? (meanAbsolute = mean) : (meanAbsolute = mean * -1);
+		sumMinMax = (min + max) * (delta + 1) / 2;
+
+		System.out.println("Sum = " + summe);
+		System.out.println("Minimum = " + min);
+		System.out.println("Maximum = " + max);
+		System.out.printf("Mean = %4.2f\n", mean);
+		System.out.printf("Reciprocal of mean = %4.2f\n", meanReciprocal);
+		System.out.printf("Additive inverse of mean = %4.2f\n", meanNegative);
+		System.out.printf("Absolute value of mean = %4.2f\n", meanAbsolute);
+		System.out.println("Delta of minimum and maximum = " + delta);
+		System.out.println("Sum of numbers from minimum to maximum = " + sumMinMax);
+
 		sc.close();
 	}
 
